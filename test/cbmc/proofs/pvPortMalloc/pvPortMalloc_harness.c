127c811f4012c9e881c7089a1031b54b6cfc0441
Gaurav Aggarwal <aggarg@amazon.com>
2022-03-03 23:53:53 +0000

    cbmc proofs
    Signed-off-by: Gaurav Aggarwal <aggarg@amazon.com>

---
+++ test/cbmc/proofs/pvPortMalloc/pvPortMalloc_harness.c
@@ -0,0 +1,47 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+/*
+ * Insert copyright notice
+ */
+#include <stddef.h>
+
+#include "FreeRTOS.h"
+
+/**
+ * @file pvPortMalloc_harness.c
+ * @brief Implements the proof harness for pvPortMalloc function.
+ */
+
+/*
+ * Insert project header files that
+ *   - include the declaration of the function
+ *   - include the types needed to declare function arguments
+ */
+void * pvPortMalloc( size_t xWantedSize );
+
+void harness()
+{
+    uint8_t app_heap[ configTOTAL_HEAP_SIZE ];
+    void *pv1, *pv2, *pv3, *pv4;
+    size_t xWantedSize1, xWantedSize2, xWantedSize3, xWantedSize4;
+
+    HeapRegion_t xHeapRegions[] =
+    {
+        { ( unsigned char * ) app_heap, sizeof( app_heap ) },
+        { NULL,                         0                  }
+    };
+    vPortDefineHeapRegions( xHeapRegions );
+
+    __CPROVER_assume( xWantedSize1 < configTOTAL_HEAP_SIZE / 4  );
+    __CPROVER_assume( xWantedSize2 < configTOTAL_HEAP_SIZE / 4  );
+    __CPROVER_assume( xWantedSize3 > configTOTAL_HEAP_SIZE / 4  );
+    __CPROVER_assume( xWantedSize3 < configTOTAL_HEAP_SIZE / 3  );
+
+    pv1 = pvPortMalloc( xWantedSize1 );
+    pv2 = pvPortMalloc( xWantedSize2 );
+    vPortFree( pv1 );
+    pv3 = pvPortMalloc( xWantedSize3 );
+    vPortFree( pv2 );
+    pv4 = pvPortMalloc( xWantedSize4 );
+}
