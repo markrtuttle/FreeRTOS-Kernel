31201220c3c093965e66ad3b21b90845b039f59d
Mark R. Tuttle <mrtuttle@amazon.com>
2022-03-14 16:40:19 -0400

    Cleanup

--- test/cbmc/proofs/pvPortMalloc/pvPortMalloc_harness.c
+++ test/cbmc/proofs/pvPortMalloc/pvPortMalloc_harness.c
@@ -22,6 +22,7 @@
  *   - include the declaration of the function
  *   - include the types needed to declare function arguments
  */
+
 void * pvPortMalloc( size_t xWantedSize );
 
 /****************************************************************/
@@ -29,10 +30,11 @@ void * pvPortMalloc( size_t xWantedSize );
 // copied from heap_5.c
 typedef struct A_BLOCK_LINK
 {
-    struct A_BLOCK_LINK * pxNextFreeBlock; /*<< The next free block in the list. */
-    size_t xBlockSize;                     /*<< The size of the free block. */
+  struct A_BLOCK_LINK * pxNextFreeBlock;
+  size_t xBlockSize;
 } BlockLink_t;
 
+// copied from heap_5.c, removed static attribute in heap_5.c
 extern BlockLink_t xStart;
 extern BlockLink_t *pxEnd;
 extern size_t xFreeBytesRemaining;
@@ -47,16 +49,16 @@ void allocate_heap(uint8_t *heap, size_t heap_size) {
   size_t offset[HEAP_BLOCKS+1];
   size_t size[HEAP_BLOCKS+1];
 
-  size_t max = 0;
+  size_t minimum_offset = 0;
   for (int i = 0; i < HEAP_BLOCKS+1; i++) {
     __CPROVER_assume((offset[i] & portBYTE_ALIGNMENT_MASK) == 0);
-    __CPROVER_assume(max <= offset[i]);
+    __CPROVER_assume(minimum_offset <= offset[i]);
     __CPROVER_assume(offset[i] <= heap_size);
 
     __CPROVER_assume(size[i] >= sizeof(BlockLink_t));
     __CPROVER_assume(size[i] <= heap_size - offset[i]);
 
-    max = offset[i] + size[i];
+    minimum_offset = offset[i] + size[i];
   }
 
   size_t free_bytes = 0;
@@ -77,13 +79,33 @@ void allocate_heap(uint8_t *heap, size_t heap_size) {
   xFreeBytesRemaining = free_bytes;
 }
 
+bool valid_heap(BlockLink_t xStart, BlockLink_t *pxEnd, size_t xFreeBytesRemaining) {
+
+  if (xStart.pxNextFreeBlock == NULL) return false;
+  if (pxEnd == NULL) return false;
+
+  size_t free_bytes = 0;
+  for (BlockLink_t *block = xStart.pxNextFreeBlock; block != pxEnd; block = block->pxNextFreeBlock) {
+    if (block > block->pxNextFreeBlock) return false;
+    if (block + block->xBlockSize > block->pxNextFreeBlock) return false;
+    free_bytes += block->xBlockSize;
+  }
+  if (xFreeBytesRemaining != free_bytes) return false;
+  return true;
+}
+
+
+
 /****************************************************************/
 
 void harness()
 {
-    uint8_t heap[ HEAP_SIZE ];
+    uint8_t heap[HEAP_SIZE];
+    size_t wanted_size;
+
     allocate_heap(&heap[0], HEAP_SIZE);
 
-    size_t wanted_size;
+    //assert(valid_heap(xStart, pxEnd, xFreeBytesRemaining));
     void *pv = pvPortMalloc(wanted_size);
+    //assert(valid_heap(xStart, pxEnd, xFreeBytesRemaining));
 }
